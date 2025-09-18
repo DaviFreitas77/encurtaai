02a7751dee3fc2a78d5254ce8e686d2ea1c77497
Eduardo Felipe da Silva <eduardo.silva338@fatec.sp.gov.br>
2025-09-18 18:06:34 -0300

    Feat: Migrats e Models

---
+++ database/migrations/2025_09_18_204052_create_urls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_url', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url_original');
+            $table->string('url_shortened')->unique();
+            $table->string('slug')->unique();
+            $table->integer('click_count')->default(0);
+            $table->enum('status', ['active', 'expired', 'inactive'])->default('active');
+            $table->unsignedInteger('fk_user');
+            $table->foreign('fk_user')->references('id')->on('tb_user')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('urls');
+    }
+};
