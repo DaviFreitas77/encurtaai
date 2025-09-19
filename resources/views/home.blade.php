ad9a82c941cdc4c014a04e72c9f5caa0e9151a4f
Davi Freitas <davifreitaz999@gmail.com>
2025-09-19 19:30:44 -0300

    add home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    aaaaaaaaaaaaaa
+
+<body class="flex">
+    <nav>
+        @include('components.sidebar')
+    </nav>
+    <main>
+        <div class="flex-1 p-10">
+            <h1 class="text-2xl font-bold">Conteúdo principal</h1>
+            <p>Texto ou componentes aqui.</p>
+        </div>
+    </main>
+
+
+
 </body>
+
 </html>
