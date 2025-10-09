1f9fa6a3629aece60afcf8fa162228a0d600aabf
Davi Freitas <davifreitaz999@gmail.com>
2025-10-09 00:02:48 -0300

    feat: adicionar novos scripts ao input do plugin Laravel no Vite

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/form-url.js','resources/js/redirect.js','resources/js/theme.js','resources/js/modals.js','resources/js/controller-dropdown.js','resources/js/sidebar.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/form-url.js','resources/js/redirect.js','resources/js/theme.js','resources/js/modals.js','resources/js/controller-dropdown.js','resources/js/sidebar.js','resources/js/segmentedControl.js','resources/js/generate-qr-code.js'],
             refresh: true,
         }),
         tailwindcss(),
