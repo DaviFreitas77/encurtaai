e8c2d29e8ccf6c39e31d22e0bff157cfa05527bd
Davifreitas77 <davifreitaz999@gmail.com>
2026-03-03 19:17:29 -0300

    config production pt/2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
+
 export default defineConfig({
     server: {
         host: '0.0.0.0', 
